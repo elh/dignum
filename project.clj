d350ab8b03bfffabec60164394af154de4909fd1
eugene huang <eugene@goforward.com>
2023-06-19 16:31:06 -0700

    setup ring-json

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Dignum Server"
   :url "https://github.com/elh/dignum"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring/ring-jetty-adapter "1.10.0"]]
+                 [ring/ring-jetty-adapter "1.10.0"]
+                 [ring/ring-json "0.5.1"]]
   :main ^:skip-aot dignum.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/dignum/core.clj
+++ src/dignum/core.clj
@@ -1,12 +1,15 @@
 (ns dignum.core
   (:gen-class)
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as util]
+            [ring.middleware.json :as ring-json]))
 
 (defn handler [_]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello World"})
+  ;; (println req)
+  (util/response {:foo "bar"}))
 
 (defn -main
   [& _]
-  (jetty/run-jetty handler {:port 3000}))
+  (jetty/run-jetty (-> handler
+                       ring-json/wrap-json-response)
+                   {:port 3000}))
