5f9051ddf70e62b5955c7975b1330a2cdff99f50
eugene huang <eugene@goforward.com>
2023-06-20 00:09:55 -0700

    add json schema validation on write

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                  [org.clojure/data.json "2.4.0"]
                  [ring/ring-jetty-adapter "1.10.0"]
                  [ring/ring-json "0.5.1"]
-                 [com.xtdb/xtdb-http-client "1.23.3"]]
+                 [com.xtdb/xtdb-http-client "1.23.3"]
+                 [jinx "0.1.6"]]
   :main ^:skip-aot dignum.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/dignum/core.clj
+++ src/dignum/core.clj
@@ -7,7 +7,8 @@
             [ring.util.response :as ring-response]
             [ring.middleware.json :as ring-json]
             [ring.middleware.params :as ring-params]
-            [xtdb.api :as xt]))
+            [xtdb.api :as xt]
+            [juxt.jinx-alpha :as jinx]))
 
 (defn- log [m]
   (println m))
@@ -26,20 +27,34 @@
       ;; no need to stringify-keys. wrap-json-response handles
       (set/rename-keys {:xt/id :id})))
 
+;; TODO: replace with real schema resources
+(def test-schema {"type" "object"
+                  "properties" {"foo" {"type" "string"}},
+                  "required" ["foo"]})
+
 (defn create-handler [xtdb-client req]
   ;; TODO: define schemas and validate schema on write
-  (let [id (.toString (java.util.UUID/randomUUID))
-        record (assoc (to-xtdb (:body req)) :xt/id id)
-        tx (xt/submit-tx xtdb-client [[::xt/put record]])]
-    (xt/await-tx xtdb-client tx)
-    (ring-response/response (to-rest record))))
+  (if (nil? (:body req))
+    {:status 400
+     :body {:message "'body' is required"}}
+    (let [validation (jinx/validate
+                      (:body req)
+                      (jinx/schema test-schema))]
+      (if (not (:valid? validation))
+        {:status 400
+         :body {:message (str "Failed validation: " (:errors validation))}}
+        (let [id (.toString (java.util.UUID/randomUUID))
+              record (assoc (to-xtdb (:body req)) :xt/id id)
+              tx (xt/submit-tx xtdb-client [[::xt/put record]])]
+          (xt/await-tx xtdb-client tx)
+          (ring-response/response (to-rest record)))))))
 
 (defn get-handler [xtdb-client req]
   (let [id (str/replace-first (:uri req) #"/" "")
         record (xt/entity (xt/db xtdb-client) id)]
     (if (nil? record)
       {:status 404
-       :body "Not Found"}
+       :body {:message "Not Found"}}
       (ring-response/response (to-rest record)))))
 
 (defn handler [xtdb-client req]
@@ -51,7 +66,7 @@
       :post (create-handler xtdb-client req)
       :get (get-handler xtdb-client req)
       {:status 501
-       :body "Unimplemented"})))
+       :body {:message "Unimplemented"}})))
 
 (defn -main []
   (let [xtdb-url (or (System/getenv "XTDB_URL") "http://localhost:9999")
