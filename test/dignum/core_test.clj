35f43a58bf7fbdbdaee697ea983d62ebdf671a8f
eugene huang <eugene@goforward.com>
2023-06-20 22:01:23 -0700

    tidy up table driven test style. add test with setup

--- test/dignum/core_test.clj
+++ test/dignum/core_test.clj
@@ -4,42 +4,44 @@
             [xtdb.api :as xt]))
 
 (deftest create-collection-test
-  (are [request res-status] (= (:status (with-open [node (xt/start-node {})]
-                                          (handler node request)))
-                               res-status)
-    ;; success
-    {:uri "/collections"
-     :request-method :post
-     :body {"_name" "collections/users"
-            "schema" {"type" "object"}}} 200
-    ;; missing schema
-    {:uri "/collections"
-     :request-method :post
-     :body {"_name" "collections/users"}} 400
-    ;; additional field
-    {:uri "/collections"
-     :request-method :post
-     :body {"_name" "collections/users"
-            "schema" {"type" "object"}
-            "other-field" "not allowed"}} 400
-    ;; missing _name
-    {:uri "/collections"
-     :request-method :post
-     :body {"schema" {"type" "object"}}} 400
-    ;; invalid _name w/o "collections/" prefix
-    {:uri "/collections"
-     :request-method :post
-     :body {"_name" "users"
-            "schema" {"type" "object"}}} 400
-    ;; cannot edit collections/collections
-    {:uri "/collections"
-     :request-method :post
-     :body {"_name" "collections/collections"
-            "schema" {"type" "object"}}} 409
-    ;; TODO: collection already exists
-    ;; schema is invalid
-    {:uri "/collections"
-     :request-method :post
-     :body {"_name" "collections/users"
-            "schema" {"type" "invalid-schema"}}} 400
-    ))
+  (let [prior-coll {"_name" "collections/teams"
+                    "schema" {"type" "object"}}]
+    (letfn [(->request [coll]
+                       {:uri "/collections"
+                        :request-method :post
+                        :body coll})]
+      (are [desc res-status coll] (= (:status (with-open [node (xt/start-node {})]
+                                                (when (not= 200 (:status (handler node (->request prior-coll))))
+                                                  (throw (Exception. "set up create failed")))
+                                                (handler node (->request coll))))
+                                     res-status)
+        "success" 200
+        {"_name" "collections/users"
+         "schema" {"type" "object"}}
+
+        "missing schema" 400
+        {"_name" "collections/users"}
+
+        "additional field" 400
+        {"_name" "collections/users"
+         "schema" {"type" "object"}
+         "other-field" "not allowed"}
+
+        "missing _name" 400
+        {"schema" {"type" "object"}}
+
+        "invalid _name w/o 'collections/' prefix" 400
+        {"_name" "users"
+         "schema" {"type" "object"}}
+
+        "cannot edit collections/collections" 409
+        {"_name" "collections/collections"
+         "schema" {"type" "object"}}
+
+        "cannot create collection that already exists" 409
+        {"_name" "collections/teams"
+         "schema" {"type" "object"}}
+
+        "schema is invalid" 400
+        {"_name" "collections/users"
+         "schema" {"type" "invalid-schema"}}))))
