17e2bbbd944a32a24ce3bd1dccd721e5f780ce81
eugene huang <eugene@goforward.com>
2023-06-20 19:10:24 -0700

    change _id to _name. emphasize the API resource
    the API resource is the real resource, not the xtdb representation

--- src/dignum/core.clj
+++ src/dignum/core.clj
@@ -30,12 +30,12 @@
 (defn- to-xtdb [record]
   (-> record
       (walk/keywordize-keys)
-      (set/rename-keys {:_id :xt/id})))
+      (set/rename-keys {:_name :xt/id})))
 
 (defn- to-rest [xt-record]
   (-> xt-record
-      (walk/stringify-keys)
-      (set/rename-keys {:xt/id :_id})))
+      (set/rename-keys {:xt/id :_name})
+      (walk/stringify-keys)))
 
 (defn- uri-parts [uri]
   (str/split (str/replace-first uri #"/" "") #"/"))
@@ -44,8 +44,9 @@
 (defn- remove-underscore-keys [m]
   (apply dissoc m (filter #(str/starts-with? % "_") (keys m))))
 
-(defn create-type [xtdb-client record]
-  (let [validation (jinx/validate
+(defn create-type [xtdb-node record]
+  (let [record (assoc record "_type" "type/type")
+        validation (jinx/validate
                     (remove-underscore-keys record)
                     (jinx/schema type-schema))]
     (cond
@@ -53,32 +54,39 @@
       {:status 400
        :body {:message (str "Failed validation: " (:errors validation))}}
 
-      (not (re-matches #"type/\w+" (get record "_id")))
+      (not (contains? record "_name"))
       {:status 400
-       :body {:message "'id' must be of the form 'type/<id>'"}}
+       :body {:message "'_name' is required and must be of the form 'type/<id>'"}}
 
-      (= (get record "_id") "type/type")
-      {:status 403
-       :body {:message "Cannot create or update type/type"}}
+      (not (re-matches #"type/\w+" (get record "_name")))
+      {:status 400
+       :body {:message "'_name' must be of the form 'type/<id>'"}}
 
-      (some? (xt/entity (xt/db xtdb-client) (get record "_id")))
+      (= (get record "_name") "type/type")
       {:status 403
-       :body {:message (str "Type already exists: " (get record "_id"))}}
+       :body {:message "Cannot create or update 'type/type'"}}
+
+      (some? (xt/entity (xt/db xtdb-node) (get record "_name")))
+      {:status 403
+       :body {:message (str "Type already exists: " (get record "_name"))}}
 
       :else
       (try
         ;; exception thrown if schema is invalid
         (jinx/schema (get record "schema"))
-        (let [xt-record (assoc (to-xtdb record) :_type "type/type")
-              tx (xt/submit-tx xtdb-client [[::xt/put xt-record]])]
-          (xt/await-tx xtdb-client tx)
+        (let [xt-record (to-xtdb record)
+              tx (xt/submit-tx xtdb-node [[::xt/put xt-record]])]
+          (xt/await-tx xtdb-node tx)
           (ring-response/response (to-rest xt-record)))
         (catch Exception e
           {:status 400
            :body {:message (str "Failed validation: " e)}})))))
 
-(defn create-record [xtdb-client type record]
-  (let [xt-type-record (xt/entity (xt/db xtdb-client) (str "type/" type))]
+(defn create-record [xtdb-node type record]
+  (let [record (-> record
+                   (assoc "_type" (str "type/" type))
+                   (assoc "_name" (str type "/" (.toString (java.util.UUID/randomUUID)))))
+        xt-type-record (xt/entity (xt/db xtdb-node) (str "type/" type))]
     (if (nil? xt-type-record)
       {:status 404
        :body {:message (str "Type not found: " type)}}
@@ -89,15 +97,11 @@
         (if (not (:valid? validation))
           {:status 400
            :body {:message (str "Failed validation: " (:errors validation))}}
-          (let [id (.toString (java.util.UUID/randomUUID))
-                xt-record (-> (to-xtdb record)
-                              (assoc :xt/id (str type "/" id))
-                              (assoc :_type (str "type/" type)))
-                tx (xt/submit-tx xtdb-client [[::xt/put xt-record]])]
-            (xt/await-tx xtdb-client tx)
-            (ring-response/response (to-rest xt-record))))))))
+          (let [tx (xt/submit-tx xtdb-node [[::xt/put (to-xtdb record)]])]
+            (xt/await-tx xtdb-node tx)
+            (ring-response/response record)))))))
 
-(defn create-handler [xtdb-client req]
+(defn create-handler [xtdb-node req]
   (if (nil? (:body req))
     {:status 400
      :body {:message "'body' is required"}}
@@ -108,38 +112,38 @@
         (let [type (first parts)
               record (:body req)]
           (case type
-            "type" (create-type xtdb-client record)
-            (create-record xtdb-client type record)))))))
+            "type" (create-type xtdb-node record)
+            (create-record xtdb-node type record)))))))
 
-(defn get-handler [xtdb-client req]
+(defn get-handler [xtdb-node req]
   (let [parts (uri-parts (:uri req))]
     (if (not= (count parts) 2)
       {:status 400
        :body {:message "Invalid uri"}}
       (let [type (first parts)
             id (second parts)
-            xt-record (xt/entity (xt/db xtdb-client) (str type "/" id))]
+            xt-record (xt/entity (xt/db xtdb-node) (str type "/" id))]
         (if (nil? xt-record)
           {:status 404
            :body {:message "Not Found"}}
           (ring-response/response (to-rest xt-record)))))))
 
-(defn handler [xtdb-client req]
+(defn handler [xtdb-node req]
   (let [req (if (is-empty-body? (:body req))
               (assoc req :body nil)
               req)]
     (log {:msg "request received" :req req})
     (case (:request-method req)
-      :post (create-handler xtdb-client req)
-      :get (get-handler xtdb-client req)
+      :post (create-handler xtdb-node req)
+      :get (get-handler xtdb-node req)
       {:status 501
        :body {:message "Unimplemented"}})))
 
 (defn -main []
   (log "Starting server")
   (let [xtdb-url (or (System/getenv "XTDB_URL") "http://localhost:9999")
-        xtdb-client (xt/new-api-client xtdb-url)]
-    (jetty/run-jetty (-> (partial handler xtdb-client)
+        xtdb-node (xt/new-api-client xtdb-url)]
+    (jetty/run-jetty (-> (partial handler xtdb-node)
                          ring-json/wrap-json-response
                          ring-json/wrap-json-body
                          ring-params/wrap-params)
