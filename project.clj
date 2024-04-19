61242e6d38596e16d24799b451d69c39d20f7cb4
eugene huang <eugene@goforward.com>
2024-04-18 21:42:33 -0700

    update deps

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/data.json "2.4.0"]
-                 [ring/ring-jetty-adapter "1.10.0"]
+                 [org.clojure/data.json "2.5.0"]
+                 [ring/ring-jetty-adapter "1.12.1"]
                  [ring/ring-json "0.5.1"]
-                 [com.xtdb/xtdb-http-client "1.23.3"]
+                 [com.xtdb/xtdb-http-client "1.24.3"]
                  [jinx "0.1.6"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.15.2"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.17.0"]
                  [com.github.java-json-tools/json-patch "1.13"]]
   :main ^:skip-aot dignum.server
   :target-path "target/%s"
